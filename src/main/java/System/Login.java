401c9298840d170a718bc90aa1ac764c8d92671a
PiotrPtasznik <piotr.ptasznik111@gmail.com>
2023-08-21 13:20:31 +0200

    Login page with "clear" button functionality

---
+++ src/main/java/System/Login.java
@@ -0,0 +1,93 @@
+package System;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+
+    JButton login, clear, signup;
+    JTextField cardTextField, pinTextField;
+
+    Login(){
+        setTitle("Login window");
+
+        setLayout(null);
+
+        ImageIcon imgIc = new ImageIcon(ClassLoader.getSystemResource("icons/logo.png"));
+        Image imgIc2 = imgIc.getImage().getScaledInstance(100, 100 , Image.SCALE_DEFAULT);
+        ImageIcon imgIc3 = new ImageIcon(imgIc2);
+
+        JLabel label = new JLabel(imgIc3);
+        label.setBounds(70,10, 100, 120);
+        add(label);
+
+        JLabel text = new JLabel("Welcome to ATM");
+        text.setFont(new Font("Calibri",Font.BOLD, 40));
+        text.setBounds(260, 60, 400, 40);
+        add(text);
+
+        JLabel cardNum = new JLabel("Card Num :");
+        cardNum.setFont(new Font("Calibri",Font.BOLD, 30));
+        cardNum.setBounds(100, 180, 400, 40);
+        add(cardNum);
+
+        cardTextField = new JTextField();
+        cardTextField.setBounds(280, 180, 250, 30);
+        add(cardTextField);
+
+        JLabel pin = new JLabel("PIN :");
+        pin.setFont(new Font("Calibri",Font.BOLD, 30));
+        pin.setBounds(100, 250, 400, 40);
+        add(pin);
+
+        pinTextField = new JTextField();
+        pinTextField.setBounds(280, 250, 250, 30);
+        add(pinTextField);
+
+        login = new JButton("Log in");
+        login.setBounds(280, 300, 120, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.white);
+        login.addActionListener(this);
+        add(login);
+
+        clear = new JButton("Clear");
+        clear.setBounds(410, 300, 120, 30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.white);
+        clear.addActionListener(this);
+        add(clear);
+
+        signup = new JButton("Sign up");
+        signup.setBounds(280, 350, 250, 30);
+        signup.setBackground(Color.BLACK);
+        signup.setForeground(Color.white);
+        signup.addActionListener(this);
+        add(signup);
+
+
+        getContentPane().setBackground(Color.white);
+
+        setSize(800,480);
+        setVisible(true);
+        setLocation(350,200);
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource() == login){
+
+        }
+        else  if (ae.getSource() == clear){
+            cardTextField.setText("");
+            pinTextField.setText("");
+        }
+        else  if (ae.getSource() == signup){
+
+        }
+
+    }
+}
+
